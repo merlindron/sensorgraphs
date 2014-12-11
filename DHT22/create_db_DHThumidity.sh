6a69d6d1dd38fa6926df0aa6bbc30d6d40ec813b
merlindron <ambrus.csaba@gmail.com>
2014-12-11 10:33:12 +0100

    Revert "v01"
    This reverts commit c6a151eadaf2c0ec69e1958696bf7c25abc4b079.

--- DHT22/create_db_DHThumidity.sh
+++
@@ -1,7 +0,0 @@
-rrdtool create DHThumidity.rrd --start N --step 300 \
-DS:DHThumidity:GAUGE:600:U:U \
-RRA:AVERAGE:0.5:1:12 \
-RRA:AVERAGE:0.5:1:288 \
-RRA:AVERAGE:0.5:12:168 \
-RRA:AVERAGE:0.5:12:720 \
-RRA:AVERAGE:0.5:288:365
